a094a3e2bb778f308706e12dc85023b25bc41b41
Swarnendu Dey <55930091+Swarnendu14@users.noreply.github.com>
2023-05-17 11:06:28 +0530

    Update

--- day_3/problem4.js
+++ day_3/problem4.js
@@ -1,3 +1,19 @@
-let arr=[5,2,8,1,3,0];
-arr.sort((a,b)=>a-b);
-console.log(arr[1]);
+let arr=[5,2,8,9,7,0];
+let min;
+let indx=0;
+for(let i=0;i<2;i++)
+{
+    min=Number.MAX_SAFE_INTEGER;
+    for(let j=0;j<arr.length-i;j++)
+    {
+        if(arr[j]<min)
+        {    
+            min=arr[j];
+            indx=j;
+        }
+    }
+    let temp=arr[indx];
+    arr[indx]=arr[arr.length-1-i];
+    arr[arr.length-1-i]=temp;
+}
+console.log(arr[arr.length-2]);
